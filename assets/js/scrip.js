1dfc5115479835fe71eeebb5c09256f66c1b1e88
danielshang11 <danielshang11@gmail.com>
2022-11-21 23:57:49 -0800

    tried to fetch

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -4,10 +4,10 @@ var temperatureEl = document.getElementById("temperature");
 var windSpeedEl = document.getElementById("wind-speed");
 var humidityEl = document.getElementById("humidity");
 var uvEl = document.getElementById("UV-index");
-var searchChar = document.getElementById("city-input")
-var searchHistory = [];
+var searchChar = document.getElementById("city-input").value;
+var searchHistory = document.getElementById("history-list");
+var searchFormEl = document.getElementById("search-form");
 
-var cityName = "Irvine";
 
 var weatherApiUrl = "https://api.openweathermap.org";
 var apiKey = "387c51dd523a8b61efaf47adae8b3466";
@@ -17,7 +17,74 @@ var apiKey = "387c51dd523a8b61efaf47adae8b3466";
 
 var searchHistorySection = document.getElementById("history");
 
-var todayWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
-var fivedaysForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",us&appid=" + apiKey + "&units=imperial";
+var todayWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchChar + "&appid=" + apiKey + "&units=imperial";
+var fivedaysForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchChar + ",us&appid=" + apiKey + "&units=imperial";
 var historyListEl = document.getElementById("history-list");
 var historyFormEl = document.getElementById("search-form");
+
+
+console.log(searchChar)
+
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    
+    if (searchChar){
+        getTodaysWeather(searchChar);
+        searchHistory.textContent="";
+        searchChar.value = "";
+    } else {
+        alert("Please enter the City Name.");
+    }
+    console.log(searchChar)
+}
+
+
+
+var getTodaysWeather = function(city){
+    var todayWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchChar + "&appid=" + apiKey + "&units=imperial";
+    fetch(todayWeatherUrl)
+    .then(function(response){
+        if(response){
+            console.log(response);
+            response.json()
+        
+        }
+    }).then(function(data){
+        console.log(data)
+})
+}
+
+
+
+searchFormEl.addEventListener('submit', formSubmitHandler);
+
+
+
+// document.addEventListener("submit",function showSearchHistory() {
+//     searchCityHistory.innerHTML = '';
+    
+//     for (var i = 0; i < searchCityHistory.length - 1; i >= 0; i--) {
+//       var button = document.createElement('button');
+//       button.setAttribute('type', 'button');
+//       // ADD CODE TO SET CLASSES OF BUTTON
+//       button.setAttribute('data-citySearch', searchCityHistory[i]);
+//       button.textContent = searchCityHistory[i];
+//       searchCityHistory.append(button);
+//     }
+// })
+
+// document.addEventListener("submit",function(event){
+//     event.preventDefault();
+//     fetch(todayWeatherUrl)
+//     .then(function(res){
+//         if(res.status != 200){
+//             window.alert="please enter the city name!"
+//             return res.json();
+//         }
+//     })
+//     .then(function(data){
+//         d
+//     })
+
+// })
